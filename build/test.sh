0bd93809aaeac48f74092aefb7d3b083cc51b277
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-05-27 14:36:44 +0530

    Updating build to ouput test report

--- build/test.sh
+++ build/test.sh
@@ -1,7 +1,8 @@
 echo "Pushing code to scratch org..."
 sfdx force:source:push -u ${CIRCLE_BRANCH}
 echo "Run all tests in scratch org..."
+echo "Getting test results..."
 testId=`sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername ${CIRCLE_BRANCH}`
-# echo "Getting test results..."
 testRunId=`echo ${testId} | grep ".-i.*" -o | cut -c 5-19`
+echo "Getting test report for " ${testRunId}
 sfdx force:apex:test:report -i ${testRunId} -u ${CIRCLE_BRANCH} -r tap -d test-report -c
