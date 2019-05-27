46532130f85f7bdceb19c02ff91b099deb327ae2
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-05-27 13:28:49 +0530

    Updating build to display test results

--- build/test.sh
+++ build/test.sh
@@ -1,4 +1,6 @@
 echo "Pushing code to scratch org..."
 sfdx force:source:push -u ${CIRCLE_BRANCH}
-echo "run all tests in scratch org..."
+echo "Run all tests in scratch org..."
 sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername ${CIRCLE_BRANCH}
+echo "Getting test results..."
+sfdx force:apex:test:report -u ${CIRCLE_BRANCH} -r tap -d test-report -c
